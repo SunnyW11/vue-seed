667833c301ec5e93ebc460e60d88b9743c24fc4e
wusonglin <490892666@qq.com>
2020-04-22 16:02:57 +0800

    feat: 自动注册base组件

---
+++ src/components/base-components/base-text.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="text">text</div>
+</template>
+
+<script>
+export default {
+  name: "base-text"
+};
+</script>
+
+<style>
+</style>
---
+++ src/utils/registerBaseComponent.js
@@ -0,0 +1,12 @@
+export default function registerBaseComponent(Vue) {
+  const requireComponent = require.context(
+    "@/components/base-components",
+    false,
+    /.(vue|js)/
+  );
+  requireComponent.keys().forEach(filename => {
+    const componentConfig = requireComponent(filename);
+    const componentName = filename.replace(/^\.\/(.*)\.\w+$/, "$1");
+    Vue.component(componentName, componentConfig.default || componentConfig);
+  });
+}
