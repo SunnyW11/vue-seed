a292aef52148ba394cf5bd2cf6c0ee812523b480
wusonglin <490892666@qq.com>
2020-04-22 15:50:02 +0800

    feat: 注入全局scss变量

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+// vue.config.js
+module.exports = {
+	css: {
+    loaderOptions: {
+      // 全局注入scss变量  免去每个页面import
+      scss: {
+        prependData: `@import "~@/style/var.scss";`
+      },
+    }
+  }
+}
